19d06d9d5b91d9c5630a7f5cf6068170714c1933
Bolatov Alau <bolatovalau@gmail.com>
2024-10-16 20:55:44 -0500

    first commit

--- obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf784187946a509fa9cb063417cea76f964dff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
