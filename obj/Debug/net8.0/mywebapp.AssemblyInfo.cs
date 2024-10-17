8532a7562744b2a5006ee8795c6754e719538858
Bolatov Alau <bolatovalau@gmail.com>
2024-10-16 21:01:56 -0500

    Feature 1 added to Program.cs2

--- obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf784187946a509fa9cb063417cea76f964dff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e30bf8ad0a7feacdee25a0bb6d9119b2ababd1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
